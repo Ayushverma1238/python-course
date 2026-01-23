4c994b5cdf4ff336ea32a429eaf6a2ce013aa3ec
Ayush Verma <vermaayush9170@gmail.com>
2026-01-23 10:55:03 +0530

    Switch case

--- switch.py
+++ switch.py
@@ -9,3 +9,12 @@ def switch(day):
     }.get(day, "Invalid day")
 
 print(switch(5))
+
+class Number:
+    def __init__(self, value):
+        self.value = value
+    def __add__(self, other):
+        return self.value + other.value
+n1 = Number(5)
+n2 = Number(10)
+print(n1 + n2)
